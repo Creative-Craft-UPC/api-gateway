bf7b35c71a6554e160d11c36905dded0bf8d45a2
Stevens Kharis Acha Esquerre <stevens.acha.13@gmail.com>
2025-06-27 20:49:18 -0500

    feat: adding cors

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from routers import education_routes, profile_education_routes, profile_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Gateway API")
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # O usa el dominio de tu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(profile_education_routes.router, prefix="/gateway/profile-educatiton", tags=["Profile_education"])
 app.include_router(profile_routes.router, prefix="/gateway/profile", tags=["Profile"])
