e7a103c25f6e2589ec3abca93f9f128edf9b0e25
Stevens Kharis Acha Esquerre <stevens.acha.13@gmail.com>
2025-06-27 21:34:35 -0500

    fix: bendito typo

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ app.add_middleware(
 )
 
 
-app.include_router(profile_education_routes.router, prefix="/gateway/profile-educatiton", tags=["Profile_education"])
+app.include_router(profile_education_routes.router, prefix="/gateway/profile-education", tags=["Profile_education"])
 app.include_router(profile_routes.router, prefix="/gateway/profile", tags=["Profile"])
 app.include_router(education_routes.router, prefix="/gateway/education",tags=["Education"])
